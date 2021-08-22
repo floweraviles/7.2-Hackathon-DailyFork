2ac6e52d58cbbb4367b0eeda05352e8181181874
Flower Aviles <itsfloweraviles@gmail.com>
2021-08-21 23:19:18 -0400

    one change

--- dailyfork/src/Pages/Home.js
+++ dailyfork/src/Pages/Home.js
@@ -4,6 +4,9 @@ import DailyRecipe from "../Components/DailyRecipe"
 function Home() {
     return (
         <div>
+           <h1>
+               Meal of the Day
+               </h1> 
            < DailyRecipe />
         </div>
     )
